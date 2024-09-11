80cf8b3cc1af11be47f97d1436a1f605525424c1
Pandie05 <hreyes@email.neit.edu>
2024-09-10 20:43:52 -0400

    done with w6 assigmnet

--- w6/week2.js
+++ w6/week2.js
@@ -3,7 +3,6 @@ function validateForm() {
     var firstName = document.getElementById("first-name");
     var lastName = document.getElementById("last-name");
     var email = document.getElementById("email");
-    var emailConfirm = document.getElementById("email-confirm");
     var phone = document.getElementById("phone");
 
     // Get error spans
@@ -17,75 +16,98 @@ function validateForm() {
     var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Basic email pattern
     var phonePattern = /^\d{10}$/; // Phone number must be 10 digits
 
-    var valid = true;
+    //confirm
+    var confirm = document.getElementById("confirmation");
 
-    // Validate First Name
-    if (!firstName.value.match(namePattern)) {
-        fnError.innerHTML = "* Please input a valid name.";
-        firstName.previousElementSibling.style.color = "red";
-        valid = false;
-    } else {
+    //val first name
+
+    if (firstName.value == "") {
+        fnError.innerHTML = "<span style='color: red;'> *</span> First name is required";
+        firstName.parentElement.classList.add("error");
+    }
+    else if (!namePattern.test(firstName.value)) {
+        fnError.innerHTML = "<span style='color: red;'> *</span> Only letters and dashes allowed";
+        firstName.parentElement.classList.add("error");
+    }
+    else {
         fnError.innerHTML = "";
-        firstName.previousElementSibling.style.color = "black";
+        firstName.parentElement.classList.remove("error");
     }
 
-    // Validate Last Name
-    if (!lastName.value.match(namePattern)) {
-        lnError.innerHTML = "* Please input a valid name.";
-        lastName.previousElementSibling.style.color = "red";
-        valid = false;
-    } else {
+    //val last name
+
+    if (lastName.value == "") {
+        lnError.innerHTML = "<span style='color: red;'> *</span> Last name is required";
+        lastName.parentElement.classList.add("error");
+    }
+    else if (!namePattern.test(lastName.value)) {
+        lnError.innerHTML = "<span style='color: red;'> *</span> Only letters and dashes allowed";
+        lastName.parentElement.classList.add("error");
+    }
+    else {
         lnError.innerHTML = "";
-        lastName.previousElementSibling.style.color = "black";
+        lastName.parentElement.classList.remove("error");
     }
 
-    // Validate Email
-    if (!email.value.match(emailPattern)) {
-        emailError.innerHTML = "* Please input a valid email address.";
-        email.previousElementSibling.style.color = "red";
-        valid = false;
-    } else {
+    //val email
+
+    if (email.value == "") {
+        emailError.innerHTML = "<span style='color: red;'> *</span> Email is required";
+        email.parentElement.classList.add("error");
+    }
+    else if (!emailPattern.test(email.value)) {
+        emailError.innerHTML = "<span style='color: red;'> *</span> Invalid email";
+        email.parentElement.classList.add("error");
+    }
+    else {
         emailError.innerHTML = "";
-        email.previousElementSibling.style.color = "black";
+        email.parentElement.classList.remove("error");
     }
 
-    // Validate Email Confirmation
-    if (emailConfirm.value !== email.value) {
-        emailError.innerHTML = "* Emails do not match.";
-        email.previousElementSibling.style.color = "red";
-        valid = false;
-    } else if (email.value.match(emailPattern)) {
-        emailError.innerHTML = "";
-        email.previousElementSibling.style.color = "black";
-    }
+    //val phone
 
-    // Validate Phone Number
-    if (!phone.value.match(phonePattern)) {
-        phoneError.innerHTML = "* Please input a valid 10-digit phone number.";
-        phone.previousElementSibling.style.color = "red";
-        valid = false;
-    } else {
+    if (phone.value == "") {
+        phoneError.innerHTML = "<span style='color: red;'> *</span> Phone is required";
+        phone.parentElement.classList.add("error");
+    }
+    else if (!phonePattern.test(phone.value)) {
+        phoneError.innerHTML = "<span style='color: red;'> *</span> Phone must be 10 digits";
+        phone.parentElement.classList.add("error");
+    }
+    else {
         phoneError.innerHTML = "";
-        phone.previousElementSibling.style.color = "black";
+        phone.parentElement.classList.remove("error");
     }
 
-    // If form is valid, hide form and show confirmation
-    if (valid) {
-        document.getElementById("form").style.display = "none"; // Hide form
-
+    //check if all fields have been submitted
+    if (firstName.value == "" || lastName.value == "" || email.value == "" || phone.value == "") {
+        //display error message
+        document.getElementById("confirmation").innerHTML = "<p style='color: red;'>Please fill out all fields</p>";
+    } 
+    else {
+        //construct an object to hold the form information
         var person = {
             fname: firstName.value,
             lname: lastName.value,
             email: email.value,
-            phone: phone.value.substring(0, 3) + "-" + phone.value.substring(3, 6) + "-" + phone.value.substring(6)
+            phone: phone.value
         };
 
-        // Show confirmation
+        //hide the form div
+        document.getElementById("form").style.display = "none";
+
+        //show the confirmation div
         document.getElementById("confirmation").style.display = "block";
-        document.getElementById("info").innerHTML = 
-            "First Name: " + person.fname + "<br>" +
-            "Last Name: " + person.lname + "<br>" +
-            "Email: " + person.email + "<br>" +
-            "Phone: " + person.phone;
+
+        //write the object's information onto the confirmation
+        document.getElementById("confirmation").innerHTML = `
+            <h2>Confirmation</h2>
+            <p>${person.fname}</p>
+            <p>${person.lname}</p>
+            <p>${person.email}</p>
+            <p>${person.phone.substring(0, 3)}-${person.phone.substring(3, 6)}-${person.phone.substring(6)}</p>
+        `;
     }
+
+
 }
